24602e60f27c83abe87f53360ea3948774150731
Izisol <shaxzod@koryeogs.com>
2025-06-09 08:42:50 +0500

    removed extranal code

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,3 @@ use App\Http\Controllers\ScrapingController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-
-Route::get('/v1/scraping/scrape-quotes', [ScrapingController::class, 'scrapeQuotes']);
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,9 @@
 
 use App\Http\Controllers\EomborController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ScrapingController;
 
 Route::get('/', function () {
     return view('e-ombor');
 });
-
-// Route::get('/quotes', [ScrapingController::class, 'viewQuotes']);
-// 
-// routes/web.php
-
-// Route::get('/quotes', [ScrapingController::class, 'viewQuotes'])->name('quotes.index');
-// Route::get('/quotes/export', [ScrapingController::class, 'export'])->name('quotes.export');
 
 Route::post('/e-ombor-login', [EomborController::class, 'loginPage'])->name('e-ombor-login');
